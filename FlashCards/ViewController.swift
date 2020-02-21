3b1eee8f3a6f33c46d10bde742c05647f395f7d9
Grant Commodore <grantcommodore24@gmail.com>
2020-02-21 14:39:07 -0500

    Adding Optional Features

--- FlashCards/ViewController.swift
+++ FlashCards/ViewController.swift
@@ -15,15 +15,58 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var question: UILabel!
     
+    @IBOutlet weak var card: UIView!
+    
+    
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        card.clipsToBounds = true
+        
+        Button1.layer.borderWidth = 2.0
+        Button1.layer.cornerRadius = 20.0
+        Button1.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        Button2.layer.borderWidth = 2.0
+        Button2.layer.cornerRadius = 20.0
+        Button2.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        Button3.layer.borderWidth = 2.0
+        Button3.layer.cornerRadius = 20.0
+        Button3.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+
     }
 
 
     @IBAction func tappedFlashCard(_ sender: Any) {
-        question.isHidden = true;
-        print("Tapped")
+        if(question.isHidden != true){
+            question.isHidden = true;
+            print("Tapped Question")
+        }else{
+            question.isHidden = false;
+        }
     }
+    
+    
+    @IBAction func button1Clicked(_ sender: Any) {
+        question.isHidden = true
+    }
+    
+    
+    @IBAction func button2Clicked(_ sender: Any) {
+        Button2.isHidden = true
+    }
+    
+    @IBAction func button3Clicked(_ sender: Any) {
+        Button3.isHidden = true
+    }
+    
     
 }
